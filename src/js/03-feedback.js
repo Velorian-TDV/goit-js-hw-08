d3dd5ed72df087f06052f41e0014ebd76bd8d683
Velorian-TDV <terleckydima92@gmail.com>
2023-05-28 20:02:39 +0300

    third task complete

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+const throttleData = throttle(dataToStorage, 500);
+const objetctForStorage = {
+    email: '',
+    message: '',
+}
+
+function dataToStorage(data) {
+
+    if (data.target.type === 'email') {
+
+        objetctForStorage.email = data.target.value;
+        localStorage.setItem('feedback-form-state', JSON.stringify(objetctForStorage))
+
+    } else if (data.target.type === 'textarea') {
+
+        objetctForStorage.message = data.target.value;
+        localStorage.setItem('feedback-form-state', JSON.stringify(objetctForStorage))
+
+    }
+
+}
+
+window.addEventListener('load', () => {
+
+    if (localStorage.getItem('feedback-form-state') !== null) {
+        
+        const dataFromStorage = JSON.parse(localStorage.getItem('feedback-form-state'))
+
+        emailInput.value = dataFromStorage.email;
+        messageInput.value = dataFromStorage.message;
+
+    }
+
+})
+
+form.addEventListener('input', throttleData)
+
+form.addEventListener('submit', () => localStorage.clear())
