1d8e16f0c1efda2c4004964cb8da5513973b6663
Velorian-TDV <terleckydima92@gmail.com>
2023-05-28 18:41:27 +0300

    second task compliete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const vimeo_player = document.getElementById('vimeo-player');
+const storageTimer = localStorage.getItem('videoplayer-current-time');
+const player = new Player(vimeo_player);
+
+function updater() {
+    player.getCurrentTime()
+        .then(data => {
+            localStorage.setItem('videoplayer-current-time', data);
+        });
+}
+
+const throttledUpdater = throttle(updater, 1000);
+
+player.on('timeupdate', throttledUpdater);
+
+if (storageTimer === null) {
+    player.setCurrentTime(0);
+} else {
+    player.setCurrentTime(storageTimer);
+}
